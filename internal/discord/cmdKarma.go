b3b69783dd7163b7c4e2a98dd04ec3f84f5144d0
Alex Muench <ammuench@gmail.com>
2024-06-25 22:47:12 -0500

    ✨ Build db methods for karma functions

--- internal/discord/cmdKarma.go
+++ internal/discord/cmdKarma.go
@@ -1,40 +1,118 @@
 package discord
 
 import (
-	// "github.com/ammuench/rerolled-bot/internal/db"
+	"database/sql"
 	"fmt"
+	"strconv"
+
+	"github.com/ammuench/rerolled-bot/internal/db"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func AddKarma(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// database := db.GetDB()
 	options := i.ApplicationCommandData().Options
 	optionUser := options[0].UserValue(s)
 
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Content: fmt.Sprintf("%v gave %v +1", FmtMentionString(i.Member.User.ID), FmtMentionString(optionUser.ID)),
-		},
-	})
+	parsedUserID, err := strconv.Atoi(optionUser.ID)
 	if err != nil {
-		LogCmdError(err, &cmdAddKarma, s, i)
+		LogCmdError(err, &cmdRemoveKarma, s, i)
+		return
+	}
+
+	newScore, err := updateUserKarma(parsedUserID, 1)
+	if err != nil {
+		LogCmdError(err, &cmdRemoveKarma, s, i)
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Error updating karma ⛔",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			LogCmdError(err, &cmdRemoveKarma, s, i)
+		}
+	} else {
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("%v gained a point! (Now at %v)", FmtMentionString(optionUser.ID), newScore),
+			},
+		})
+		if err != nil {
+			LogCmdError(err, &cmdRemoveKarma, s, i)
+		}
 	}
 }
 
 func RemoveKarma(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	// database := db.GetDB()
 	options := i.ApplicationCommandData().Options
 	optionUser := options[0].UserValue(s)
-
-	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: &discordgo.InteractionResponseData{
-			Content: fmt.Sprintf("%v gave %v -1", FmtMentionString(i.Member.User.ID), FmtMentionString(optionUser.ID)),
-		},
-	})
+	parsedUserID, err := strconv.Atoi(optionUser.ID)
 	if err != nil {
 		LogCmdError(err, &cmdRemoveKarma, s, i)
+		return
+	}
+
+	newScore, err := updateUserKarma(parsedUserID, -1)
+	if err != nil {
+		LogCmdError(err, &cmdRemoveKarma, s, i)
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: "Error updating karma ⛔",
+				Flags:   discordgo.MessageFlagsEphemeral,
+			},
+		})
+		if err != nil {
+			LogCmdError(err, &cmdRemoveKarma, s, i)
+		}
+	} else {
+		err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("%v lost a point (Now at %v)", FmtMentionString(optionUser.ID), newScore),
+			},
+		})
+		if err != nil {
+			LogCmdError(err, &cmdRemoveKarma, s, i)
+		}
+	}
+}
+
+type UserKarma struct {
+	userID int
+	score  int
+}
+
+func updateUserKarma(userID int, adjustmentAmt int) (int, error) {
+	database := db.GetDB()
+
+	var userKarmaState UserKarma
+
+	userLookup := database.QueryRow("SELECT * FROM karma_points WHERE userID = ?", userID)
+	err := userLookup.Scan(&userKarmaState.userID, &userKarmaState.score)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			_, updateErr := database.Exec("INSERT INTO karma_points (userID, score) VALUES (?, ?);", userID, adjustmentAmt)
+			if updateErr != nil {
+				return 0, updateErr
+			}
+
+			return adjustmentAmt, nil
+		}
+
+		return 0, err
+	} else {
+
+		newUserScore := userKarmaState.score + adjustmentAmt
+
+		_, updateErr := database.Exec("UPDATE karma_points SET score = ? WHERE userID = ?", &newUserScore, &userKarmaState.userID)
+		if updateErr != nil {
+			return 0, updateErr
+		}
+
+		return newUserScore, nil
 	}
 }
